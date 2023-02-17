1e43a81ee8d24a474a658cc6f27c68b0dbf44554
Progna_Labony_Roy <labony.progna@gmail.com>
2023-02-17 14:15:28 +0600

    feedback added

--- src/components/RatingOne/RatingOne.js
+++ src/components/RatingOne/RatingOne.js
@@ -3,15 +3,19 @@ import './RatingOne.css'
 
 const RatingOne = () => {
     return (
-        <div className='rating-one'>
+        <div className='rating-one rounded-xl'>
             <section className="rating-img">
-                <img src="https://www.boloforms.com/_next/static/media/repa.483a1471.jpeg" alt="" className='h-24 rounded-full'/>
+                <img src="https://www.boloforms.com/_next/static/media/repa.483a1471.jpeg" alt="" className='mt-4 h-24 w-24 rounded-full'/>
             </section>
             <div className="feedback">
-                <div className="rating">
-
+                <div className="rating flex">
+                <i class="text-yellow-400 fa-solid fa-star"></i>
+                <i class="text-yellow-400 fa-solid fa-star"></i>
+                <i class="text-yellow-400 fa-solid fa-star"></i>
+                <i class="text-yellow-400 fa-solid fa-star"></i>
+                <i class="text-yellow-400 fa-solid fa-star"></i> 
                 </div>
-                <p className='text-sm'>"Boloforms has been an absolute lifesaver for me. It makes workflow tasks much easier and helps to ensure that everything runs smoothly. I love how it automates tasks and eliminates manual processes, saving me time and effort. It's a great Google Workflow software and is well worth the money!"</p>
+                <p className='text-sm py-4'>"Boloforms has been an absolute lifesaver for me. It makes workflow tasks much easier and helps to ensure that everything runs smoothly. I love how it automates tasks and eliminates manual processes, saving me time and effort. It's a great Google Workflow software and is well worth the money!"</p>
                 <p className='font-bold'>Repa Mandala</p>
             </div>
         </div>
